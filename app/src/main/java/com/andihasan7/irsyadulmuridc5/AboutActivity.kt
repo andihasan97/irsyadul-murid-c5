7ca91d2aa57745f189faf9e2913640a7c5651a5b
andihasan97 <andihasanelfalakiy@gmail.com>
2022-12-24 15:00:37 +0700

    added Tengah Malam & some Activity

---
+++ app/src/main/java/com/andihasan7/irsyadulmuridc5/AboutActivity.kt
@@ -0,0 +1,5 @@
+package com.andihasan7.irsyadulmuridc5
+
+class AboutActivity {
+    
+}
---
+++ app/src/main/java/com/andihasan7/irsyadulmuridc5/CustomHisabActivity.kt
@@ -0,0 +1,8 @@
+package com.andihasan7.irsyadulmuridc5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomHisabActivity{
+    
+}

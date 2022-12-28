4ce5020069f9127638cbb87312b262e4ec2a7101
andihasan97 <andihasanelfalakiy@gmail.com>
2022-12-28 18:45:57 +0700

    add custom layout, need style improvement

--- app/src/main/java/com/andihasan7/irsyadulmuridc5/CustomHisabActivity.kt
+++ app/src/main/java/com/andihasan7/irsyadulmuridc5/CustomHisabActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import com.andihasan7.irsyadulmuridc5.databinding.ActivityCustomHisabBinding
 
 class CustomHisabActivity : AppCompatActivity() {
+    
     private lateinit var binding: ActivityCustomHisabBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCustomHisabBinding.inflate(layoutInflater)
